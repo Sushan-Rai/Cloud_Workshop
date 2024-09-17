974814f969bb5a82833364210c0bff973ea3ce85
tsshamanth <shamantht457@gmail.com>
2024-09-17 10:40:03 +0530

    pls work

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-var i = "Shamant";
+var i = "Shamanth";
 var j = "Venkatesh";
 console.log(i+j);
 console.log("loadbalancers v1");
 console.log("loadbalancers v2");
-<<<<<<< HEAD
-console.log("loadbalancers v3");
-=======
 console.log("loadbalancers v3");
 
->>>>>>> 9427db41bfd01997842ed225cbddedadc289b547
